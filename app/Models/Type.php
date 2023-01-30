ddbc004326226ca270bef88a3c488f717fa2275b
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-30 16:12:43 +0100

    refactoring

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -10,7 +10,7 @@ class Type extends Model
 {
     use HasFactory;
     protected $guarded = ['slug'];
-    public function project()
+    public function projects()
     {
         return $this->hasMany(Project::class);
     }
