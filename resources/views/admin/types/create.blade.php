83ce2c1014e9b4863e7de263060bdee345ed9f17
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-30 21:35:27 +0100

    aggiunge store alla type

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="container">
-    <h1>Dashboard</h1>
+    <h1>Aggiungi una Tipologia</h1>
+    @include('partials.error')
+    <form action="{{route('admin.types.store')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Aggiungi il Tipologia per il progetto" value="{{old('name')}}">
+        </div>
+        <button type="submit" class="btn btn-success">Salva</button>
+    </form>
 </div>
 @endsection
