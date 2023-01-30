6d911737f9bc29b193e4cca4cd952c981c12f669
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-30 17:35:29 +0100

    refactoring

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -49,7 +49,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    Sicuro di voler eliminare il progetto {{$type->title}}?
+                    Sicuro di voler eliminare la tipologia {{$type->name}}?
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
